aa8224438e1c26d882fe2a7f512591841c0b1ab9
Leo P <shell@slact.net>
2011-09-05 10:42:15 -0400

    sorting works too, now.

--- redis_index.rb
+++ redis_index.rb
@@ -14,36 +14,38 @@ module RedisIndex
       @attribute ||= @name
       @attribute = @attribute.to_sym unless !@attribute
       @name = @name.to_sym
+      @key ||= :id #object's key attribute (default is 'id')
+      @keyf ||= "%s#{self.class.name.sub /^.*::/, ""}:#{@name}=%s"
       if block_given?
         yield self, arg 
       end
       raise Exception, "Index must have a name" unless @name
-      @model.redis_indices[@name] = self
+      @model.add_redis_index self
     end
-    def val(val)
-      @value.nil? ? val : @value.call(val)
+    def val(value)
+      @value.nil? ? value : @value.call(value)
     end
     def digest(value)
-      Digest::SHA1.hexdigest val(value).to_s
+      Digest::SHA1.hexdigest value.to_s
     end
     def value_is(obj)
-      val obj.send @attribute
+      obj.send @attribute
     end
     def value_was(obj)
-      val obj.send "#{@attribute}_was"
+      obj.send "#{@attribute}_was"
     end
     def update(obj)
       val_is, val_was = value_is(obj), value_was(obj)
       if(val_is != val_was)
         remove(val_was, obj)
-        add(val_is, obj)
+        add(obj)
       end
     end
     def create(obj)
-      add(value_is(obj), obj)
+      add(obj)
     end
     def delete(obj)
-      remove(value_was(obj), obj)
+      remove(obj, value_was(obj))
     end
     
   end
@@ -52,20 +54,17 @@ module RedisIndex
     def initialize(arg={})
       super arg
       @type ||= "string"
-      @key ||= :id #object's key attribute (default is 'id')
-      @set_keyf ||= "%s#{self.class.name.sub /^.*::/, ""}:#{@name}=%s"
       raise Exception, "Model not passed to index." unless @model
     end
     
-    def set_key(val, prefix=nil)
-      val = @value.call val if @value
-      @set_keyf %[prefix || @redis_prefix || @model.redis_prefix, digest(val)]
+    def set_key(value, prefix=nil)
+      @keyf %[prefix || @redis_prefix || @model.redis_prefix, digest(val value)]
     end
-    def add(val, obj)
-      @redis.sadd set_key(val), obj.send(@key)
+    def add(obj, val = nil)
+      @redis.sadd set_key(val || obj.send(@attribute)), obj.send(@key)
     end
-    def remove(val, obj)
-      @redis.sremove set_key(val), obj.send(@key)
+    def remove(obj, val = nil)
+      @redis.sremove set_key(val || obj.send(@attribute)), obj.send(@key)
     end
   end
   
@@ -96,21 +95,32 @@ module RedisIndex
   end
   
   class SortIndex < Index
-    attr_accessor :sort_by
     def initialize(arg)
       super arg
-      @hash_keyf = "#{model.name.sub /^.*::/, ""}:%s"
     end
-    def hash_key(val)
-      @hash_keyf % digest(val)
+    
+    def hash_key(val, prefix=nil)
+      @keyf % [prefix || @redis_prefix || @model.redis_prefix, val]
     end
-    def add(val, obj)
-      @redis.hset hash_key(val), @name, value_is(obj)
+    
+    def add(obj, value=nil)
+      @redis.hset hash_key(obj.send @key), @name, val(value || value_is(obj))
     end
-    def remove(val)
-      hkey = hash_key val
+    alias create add
+    
+    def remove(obj, value=nil)
+      hkey = hash_key(obj.send @key)
       @redis.hdel hkey, @name
       @redis.del hkey unless @redis.hlen(hkey) > 0
+    end
+    alias delete remove
+    
+    def update(obj)
+      add(obj) if value_is(obj) != value_was(obj)
+    end
+    
+    def sort_by
+      hash_key "*->#{@name}"
     end
   end
   
@@ -134,6 +144,7 @@ module RedisIndex
     
     def query(force=nil)
       temp_set = "#{@redis_prefix}temp_set:(#{results_key})"
+      @sort_options[:store] ||= results_key
       if force || !@redis.exists(results_key)
         @redis.del temp_set if force
         first = @queue.first
@@ -144,13 +155,12 @@ module RedisIndex
             @redis.send q[:operation], temp_set, *q[:key]
           end
         end
+        @redis.expire temp_set, 10 #10-second search set timeout
+        @redis.sort temp_set, @sort_options
+        @redis.expire @cache_key, 5*60    
+      else
+        @redis.sort results_key, @sort_options
       end
-      @redis.expire @temp_set, 10 #10-second search set timeout
-      
-      @sort_options[:store] ||= results_key
-      @redis.sort temp_set, @sort_options
-      
-      @redis.expire @cache_key, 5*60 
       self
     end
     
@@ -162,12 +172,15 @@ module RedisIndex
       res
     end
     
-    def sort(opts)
-      
+    def sort(index_name, direction=:asc)
+      index = @model.redis_index(index_name, SortIndex)
+      @sort_options[:by] = index.sort_by
+      @sort_options[:order] = direction.to_s
       self
     end
 
     def build_query(op, index, value)
+      
       @results_key = nil
       if @queue.length == 0 || @queue.last[:operation]!=op
          @queue.push :operation => op, :index => [], :value => [], :key =>[], :results_key => []
@@ -204,20 +217,30 @@ module RedisIndex
         @model.find id
       end
     end
-    def get_index(index_name)
-      @model.redis_indices[index_name.to_sym]
-    end
     def build_query(op, index_name, value)
-       index = get_index index_name
-       super op, index, value
+      index = @model.redis_index index_name
+      super op, index, value
     end
   end
   
   def self.included(base)
     base.class_eval do
       class << self
-        def redis_indices
-          @redis_indices||={}
+        def redis_index (index_name=nil, index_class = Index)
+          raise ArgumentError, "#{index_class} must be a subclass of RedisIndex::Index" unless index_class <= Index
+          if index_name.kind_of? index_class
+            return index_name 
+          elsif index_name.nil?
+            return @redis_indices 
+          end
+          @redis_indices||=[] #this line sucks.
+          index = @redis_indices.find { |i| i.name == index_name.to_sym && i.kind_of?(index_class)}
+          raise Exception, "Index #{index_name} not found in #{name}" unless index
+          index
+        end
+        def add_redis_index(index)
+          @redis_indices||=[]
+          @redis_indices.push index
         end
         def redis_prefix
           @redis_prefix||="Rails:#{Rails.application.class.parent.to_s}:#{self.name}:"
@@ -238,12 +261,12 @@ module RedisIndex
     def index_attribute(arg={}, &block)
       index_class = arg[:index] || SearchIndex
       
-      raise ArgumentError "index argument must be in RedisIndex::Index if given" unless index_class <= Index
+      raise ArgumentError, "index argument must be in RedisIndex::Index if given" unless index_class <= Index
       index_class.new(arg.merge(:model => self), &block)
     end
     
     def index_attribute_for(arg)
-      raise ArgumentError, "index_attribute_for requires :model argument" unless arg[:model]
+      raise ArgumentError ,"index_attribute_for requires :model argument" unless arg[:model]
       index = index_attribute(arg) do |index|
         index.name = "foreign_index_#{index.name}"
       end
@@ -280,9 +303,8 @@ module RedisIndex
     
     def build_redis_indices   
       self.find(:all).each { |row| row.create_redis_indices }
-      
       #update all foreign indices
-      redis_indices.each do |k, index|
+      redis_index.each do |index|
         if index.kind_of? ForeignIndex
           index.real_index.model.send :build_redis_indices 
         end
@@ -293,7 +315,7 @@ module RedisIndex
   
   [:create, :update, :delete].each do |op|
     define_method "#{op}_redis_indices" do
-      self.class.redis_indices.each { |i, index| index.send op, self}
+      self.class.redis_index.each { |index| index.send op, self}
     end
   end
 end
