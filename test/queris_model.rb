5cfa61c065230e3d2acbedb6bbc0a46c8d63dc21
Leo P <shell@slact.net>
2012-03-16 04:27:49 -0400

    testscript update (rudimentary profiling test)

--- test/queris_model.rb
+++ test/queris_model.rb
@@ -1,26 +1,25 @@
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../lib"))
 require "redis"
 require "queris"
-Queris.use_redis Redis.new(:host => 'localhost', :port => 6379, :db => 13)
 
 class Foo < Queris::Model
   attrs :a, :b, :c
-  expire 1024
-  def initialize
-    
-  end
-  
+  redis Redis.new(:host => 'localhost', :port => 6379, :db => 13)
+  profile_queries
+  index_only
+  index_attributes :a, :b, :index => Queris::DecayingAccumulatorIndex, :half_life => 604800
 end
 
 def test
-  
-
-
   f = Foo.new
-
   f.a = 11
   f.b = 9
   f.save
+  
+  q = Foo.query.union(:a, 11).diff(:a, 12)
+  
+  pr = Queris::Profiler.find(q)
+  
   binding.pry
 end
 test
