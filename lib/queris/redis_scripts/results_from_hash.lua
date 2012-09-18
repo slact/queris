515d27df11286efb15dec53020925b398c1e031f
Leo P <junk@slact.net>
2012-09-18 14:01:04 -0400

    fix: results_from_hash script for sets

--- lib/queris/redis_scripts/results_from_hash.lua
+++ lib/queris/redis_scripts/results_from_hash.lua
@@ -1,6 +1,11 @@
 local results_key = KEYS[1]
 local command, min, max, hashkeyf = ARGV[1], ARGV[2], ARGV[3], ARGV[4]
-local ids = redis.call(command, results_key, min, max)
+local ids
+if command == "smembers" then
+  ids = redis.call(command, results_key)
+else
+  ids = redis.call(command, results_key, min, max)
+end
 local ret, notfound = {}, {}
 for i,id in ipairs(ids) do
   local flathash = redis.call("hgetall", hashkeyf:format(id))
