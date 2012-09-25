4489edfffe771e85df4fb08f4396ebd9709f75f9
Leo P <junk@slact.net>
2012-09-25 16:04:21 -0400

    more informative copy_ttl script

--- lib/queris/redis_scripts/copy_ttl.lua
+++ lib/queris/redis_scripts/copy_ttl.lua
@@ -1,3 +1,13 @@
-local ttl = redis.call("ttl", KEYS[1])
-redis.call("expire", KEYS[2], ttl)
+if redis.call('exists', KEYS[2]) == 0 then
+  return nil
+end
+local ttl, delete_if_absent = redis.call("ttl", KEYS[1]), (ARGV[1] or false)
+if ttl ~= -1 then
+  redis.call("expire", KEYS[2], ttl)
+  return ttl
+elseif delete_if_absent and redis.call('exists', KEYS[1]) == 0 then
+  if redis.call("del", KEYS[2]) == 1 then
+    return "DELETED"
+  end
+end
 return 'OK'
