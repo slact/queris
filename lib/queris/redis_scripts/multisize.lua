2a02d9e7d94ba92f389c17dc2b6442b7f09e0861
Leo P <junk@slact.net>
2012-10-01 19:36:03 -0400

    better multisize script

--- lib/queris/redis_scripts/multisize.lua
+++ lib/queris/redis_scripts/multisize.lua
@@ -5,6 +5,9 @@ if keytype=='set' then
   return redis.call('scard', key)
 elseif keytype=='zset' then
   return redis.call('zcard', key)
+elseif keytype=='hash' then
+  return redis.call('hlen', key)
 else
+  redis.log(redis.LOG_WARNING, "I wonder what the size of key " .. key .. "(" .. keytype .. ")".. "is?")
   return "WTF?"
 end
