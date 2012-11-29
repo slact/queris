cb5f1b8061c3e9a23234ee3ed839024291f5189f
Leo P <junk@slact.net>
2012-11-29 18:52:33 -0500

    more flexible sizing script (multisize)

--- lib/queris/redis_scripts/multisize.lua
+++ lib/queris/redis_scripts/multisize.lua
@@ -5,9 +5,15 @@ if keytype=='set' then
   return redis.call('scard', key)
 elseif keytype=='zset' then
   return redis.call('zcard', key)
+elseif keytype=='list' then
+  return redis.call('llen', key)
 elseif keytype=='hash' then
   return redis.call('hlen', key)
+elseif keytype=='none' then
+  return 0
+elseif keytype=='string' then
+  return redis.call('strlen', key)
 else
   redis.log(redis.LOG_WARNING, "I wonder what the size of key " .. key .. "(" .. keytype .. ")".. "is?")
-  return "WTF?"
+  return "WTF for " .. keytype
 end
